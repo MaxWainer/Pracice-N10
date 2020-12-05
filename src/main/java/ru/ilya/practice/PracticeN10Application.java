1b6e069738faef3e91909182208254cbbcdf12a4
Ilya Koreisha <koreisha2000@mail.ru>
2020-12-05 19:29:05 +0300

    Initial commit

---
+++ src/main/java/ru/ilya/practice/PracticeN10Application.java
@@ -0,0 +1,13 @@
+package ru.ilya.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeN10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeN10Application.class, args);
+	}
+
+}
